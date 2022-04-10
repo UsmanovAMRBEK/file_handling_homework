2e6e397bc0b206a8c21d760557fdbd9b6cbfa6c9
unknown <princariens@gmail.com>
2022-04-10 16:11:26 +0500

    01

--- file01.py
+++ file01.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    l=[]
+    for i in data.split(','):
+        l.append(int(i))
+    return l
 
 # Read data from file
+f=open(".\\txt_file\data01.txt","r")
+data=f.read()
+print(main(data))
+f.close()
