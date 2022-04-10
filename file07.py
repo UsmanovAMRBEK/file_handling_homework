a838f407f2894207fd7ceb9d26e4cbaec7acb070
unknown <princariens@gmail.com>
2022-04-10 16:28:40 +0500

    07

--- file07.py
+++ file07.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    sum = 0
+    for i in data:
+        if i.isdigit():
+            sum += int(i)
+    return sum
     
 # Read data from file
+f=open(".\\txt_file\data07.txt","r")
+data=f.read()
+print(main(data))
+f.close()
