89c8745193f606b21fc65f30ebd7300bdbce6e4c
unknown <princariens@gmail.com>
2022-04-10 16:32:13 +0500

    08

--- file08.py
+++ file08.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    answer = 0
+    for char in data:
+        if char.isdigit():
+            if int(char) > answer:
+                answer = int(char)
+    return answer
 
 # Read data from file
+f=open(".\\txt_file\data08.txt","r")
+data=f.read()
+print(main(data))
+f.close()
