e73566b17d7edf4b025147a961a31f754aec771f
unknown <princariens@gmail.com>
2022-04-10 16:19:14 +0500

    04

--- file04.py
+++ file04.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    l=[]
+    for i in data:
+        if i.isdigit():
+            continue
+        else:
+            l.append(i)
+    return l
 # Read data from file
+f=open(".\\txt_file\data04.txt","r")
+data=f.read()
+print(main(data))
+f.close()
