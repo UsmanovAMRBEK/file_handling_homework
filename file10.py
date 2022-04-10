eb070dfd10d54ad5e72170ff3771de629dd4a53b
unknown <princariens@gmail.com>
2022-04-10 16:47:21 +0500

    10

--- file10.py
+++ file10.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    data = data.split('\n')
+    ans = 0
+    for row in data:
+        if len(row) > ans:
+            ans = len(row)
+    return ans
 
 # Read data from file
+f=open(".\\txt_file\data10.txt","r")
+data=f.read()
+print(main(data))
+f.close()
