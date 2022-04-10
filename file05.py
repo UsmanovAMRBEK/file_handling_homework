d67eedff47e50febf9de4197f106c602433dd6e4
unknown <princariens@gmail.com>
2022-04-10 16:22:48 +0500

    05

--- file05.py
+++ file05.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    digital = 0
+    non_digital = 0
+    for i in data:
+        if i.isdigit():
+            digital += 1
+        else:
+            non_digital += 1
+    return [digital, non_digital]
 # Read data from file
+f=open(".\\txt_file\data05.txt","r")
+data=f.read()
+print(main(data))
+f.close()
